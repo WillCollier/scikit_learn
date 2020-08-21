3830fc08baea3d5664fc4fefec991042ff3e17d5
WillCollier <61185593+WillCollier@users.noreply.github.com>
2020-08-21 13:57:26 +0100

    Idenitfy written numbers

---
+++ convolutional_network.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mnist
+from PIL import Image
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+
+
+# training variables
+x_train_ = mnist.train_images()
+y_train = mnist.train_labels()
+
+# testing variables
+x_test_ = mnist.test_images()
+y_test = mnist.test_labels()
+
+print("x_train: {}".format(x_train_))
+print("x_test: {}".format(x_test_))
+print("y_train: {}".format(y_train))
+print("x_train dimensions: {}".format(x_train_.ndim))
+print("x_train shape: {}".format(x_train_.shape))
+
+plt.figure()
+plt.imshow(x_train_[7000])
+plt.show()
+
+# flatten the array of inputs for use in the neural network
+x_train = x_train_.reshape((-1, 28 * 28))
+x_test = x_test_.reshape((-1, 28 * 28))
+
+# normalise the inputs to between 0 and 1, as a preferable input format
+
+x_train = np.array(x_train) / 256
+x_test = np.array(x_test) / 256
+
+# classifier
+clf = MLPClassifier(solver='adam', activation='relu', hidden_layer_sizes=(64, 64))
+
+clf.fit(x_train, y_train)
+
+predictions = clf.predict(x_test)
+
+acc = confusion_matrix(y_test, predictions)
+
+print("Accuracy: {}".format(acc))
+
+
+def accuracy(cm):
+    diagonal = cm.trace()
+    elements = cm.sum()
+    return diagonal / elements
+
+
+print("Accuracy: {}".format(accuracy(acc)))
+
+img = Image.open('five.png')
+data = list(img.getdata())
+for i in range(len(data)):
+    data[i] = 255 - data[i]
+# print(data)
+
+five = np.array(data) / 256
+
+p = clf.predict([five])
+print(p)
+
+img = Image.open('three.png')
+data = list(img.getdata())
+for i in range(len(data)):
+    data[i] = 255 - data[i]
+# print(data)
+
+three = np.array(data) / 256
+
+p = clf.predict([three])
+print(p)
